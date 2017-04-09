94da6e8679d0abc5d68522a255c7851b8de63d3d
TVke <thomas.verhelst@student.kdg.be>
2017-04-10 01:16:48 +0200

    added basic html to vrijetijd

--- resources/views/vrijetijd.blade.php
+++ resources/views/vrijetijd.blade.php
@@ -3,5 +3,59 @@
 @section('page', 'vrije tijd')
 
 @section('content')
-
+	<filter>
+		<form action="">
+			{{ csrf_field() }}
+			<h5>Categoriën</h5>
+			<label><input type="checkbox">bla</label>
+			<label><input type="checkbox">bla</label>
+			<label><input type="checkbox">bla</label>
+		</form>
+	</filter>
+	<ul>
+		<li>
+			<a href="http://henm.be">
+				<figure>
+					<img src="H&M.png" alt="H&M">
+					<figcaption>
+						<h5>H&M</h5>
+						<p>blablablablabla blablablalbalbal</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="http://henm.be">
+				<figure>
+					<img src="H&M.png" alt="H&M">
+					<figcaption>
+						<h5>H&M</h5>
+						<p>blablablablabla blablablalbalbal</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="http://henm.be">
+				<figure>
+					<img src="H&M.png" alt="H&M">
+					<figcaption>
+						<h5>H&M</h5>
+						<p>blablablablabla blablablalbalbal</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+		<li>
+			<a href="http://henm.be">
+				<figure>
+					<img src="H&M.png" alt="H&M">
+					<figcaption>
+						<h5>H&M</h5>
+						<p>blablablablabla blablablalbalbal</p>
+					</figcaption>
+				</figure>
+			</a>
+		</li>
+	</ul>
 @endsection
