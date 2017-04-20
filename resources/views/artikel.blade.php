eb240e74c574d8e4bb3205fbafb34e5291770033
Sander Borret <sander.borret@student.kdg.be>
2017-04-20 13:11:51 +0200

    added database data to schools page

--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -1,15 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($articles as $article)
-        @if($article['id'] == $id)
-        	<article>
-    			<img src="{{ $article['image_url'] }}" alt="{{ $article['image_alt'] }}">
-    			<div>
-    				<h5>{{ $article['title'] }}</h5>
-    				<p>{{ $article['content'] }}</p>
-    			</div>
-        	</article>
-        @endif
-    @endforeach
+	<article>
+		<img src="{{ $article->image_url }}" alt="{{ $article->title }}">
+		<div>
+			<h5>{{ $article->title }}</h5>
+			<p>{{ $article->content }}</p>
+		</div>
+	</article>
 @endsection
