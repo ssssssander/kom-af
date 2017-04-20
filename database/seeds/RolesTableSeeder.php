af40b5dc06caafd9dfdaef75a1e7501205fadd63
Sander Borret <sander.borret@student.kdg.be>
2017-04-20 13:41:17 +0200

    changed seeders, may not work

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Role;
 use Illuminate\Database\Seeder;
 
 class RolesTableSeeder extends Seeder
@@ -11,7 +12,7 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('roles')->insert([
+        Role::create([
             ['name' => 'editor'],
             ['name' => 'approver'],
             ['name' => 'admin'],
