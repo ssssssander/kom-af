ec0a15ae8e685ed02bb9afa1a56b9bfaaf4772f0
TVke <thomas.verhelst@student.kdg.be>
2017-04-25 01:30:54 +0200

    added search style + logo intigration

--- database/seeds/SchoolsTableSeeder.php
+++ database/seeds/SchoolsTableSeeder.php
@@ -12,11 +12,11 @@ class SchoolsTableSeeder extends Seeder
     public function run()
     {
         DB::table('schools')->insert([
-            ['name' => 'Karel de Grote Hogeschool', 'image_url' => 'lgo KdG.jpg', 'website' => 'https://www.kdg.be/'],
+            ['name' => 'Karel de Grote Hogeschool', 'image_url' => 'logo KdG.jpg', 'website' => 'https://www.kdg.be/'],
             ['name' => 'Universiteit Antwerpen', 'image_url' => 'logoUA.png', 'website' => 'https://www.uantwerpen.be/nl/'],
             ['name' => 'Thomas More', 'image_url' => 'logoThomas More.png', 'website' => 'http://www.thomasmore.be/'],
             ['name' => 'AP Hogeschool Antwerpen', 'image_url' => 'logo AP.png', 'website' => 'https://www.ap.be/'],
-            ['name' => 'Hogere Zeevaartschool Antwerpen', 'image_url' => 'logoAMA.jpg', 'website' => 'https://www.hzs.be/nl/'],
+            ['name' => 'Hogere Zeevaartschool Antwerpen', 'image_url' => 'logoAMA.png', 'website' => 'https://www.hzs.be/nl/'],
             ['name' => 'Institute of Tropical Medicine Antwerp', 'image_url' => 'logoITM.png', 'website' => 'https://www.itg.be/']
         ]);
     }
