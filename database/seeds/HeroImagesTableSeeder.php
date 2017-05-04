7217166fddc27de6cc47ca5c251f79901de8e6fc
Sander Borret <sander.borret@student.kdg.be>
2017-05-04 13:24:16 +0200

    added hero images table and basic implementation

--- database/seeds/HeroImagesTableSeeder.php
+++ database/seeds/HeroImagesTableSeeder.php
@@ -12,11 +12,11 @@ class HeroImagesTableSeeder extends Seeder
     public function run()
     {
         DB::table('hero_images')->insert([
-            ['template_name' => 'algemeen', 'hero_image_url' => 'pexels-photo-103127.jpeg'],
-            ['template_name' => 'testimonials', 'hero_image_url' => 'pexels-photo-296881.jpeg'],
-            ['template_name' => 'scholen', 'hero_image_url' => 'pexels-photo-66363.jpeg'],
-            ['template_name' => 'nieuws', 'hero_image_url' => 'pexels-photo-196667.jpeg'],
-            ['template_name' => 'gids', 'hero_image_url' => 'pexels-photo-29672.jpg'],
+            ['template_name' => 'algemeen', 'hero_image_url' => 'pexels-photo-103127-min.jpeg'],
+            ['template_name' => 'testimonials', 'hero_image_url' => 'pexels-photo-296881-min.jpeg'],
+            ['template_name' => 'scholen', 'hero_image_url' => 'pexels-photo-66363-min.jpeg'],
+            ['template_name' => 'nieuws', 'hero_image_url' => 'pexels-photo-196667-min.jpeg'],
+            ['template_name' => 'gids', 'hero_image_url' => 'pexels-photo-29672-min.jpg'],
         ]);
     }
 }
