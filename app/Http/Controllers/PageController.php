c060e558c12d7149a66c84a2b30a578b9e76daf3
Sander Borret <sander.borret@student.kdg.be>
2017-05-07 22:47:20 +0200

    scraped AP courses

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,62 +12,71 @@ use App\HeroImage;
 use Goutte\Client;
 use Illuminate\Support\Facades\DB;
 
-class PageController extends Controller
-{
+class PageController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-   public function __construct()
-   {
+    public function __construct() {
         $heroImages = HeroImage::all();
         view()->share('heroImages', $heroImages);
         // $this->middleware('auth');
-   }
+    }
 
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
      */
-	public function algemeen()
-	{
+	public function algemeen() {
 		$news = false; //DB::table("articles")->latest()->first();
 		return view('algemeen',compact('news'));
-	}
-	public function testimonials()
-	{
-		$testimonials = Testimonial::all();
+    }
 
+	public function testimonials() {
+		$testimonials = Testimonial::all();
 		return view('testimonials', compact('testimonials'));
 	}
-	public function testimonial(Testimonial $testimonial)
-	{
+
+	public function testimonial(Testimonial $testimonial) {
 		return view('testimonial', compact('testimonial'));
 	}
-	public function scholen()
-	{
-        $schools = School::all();
 
+	public function scholen() {
+        $schools = School::all();
 		return view('scholen', compact('schools'));
 	}
-	public function school(School $school)
-	{
+
+	public function school(School $school) {
         Course::where('school_id', $school->id)->delete();
 
-        if($school->id == 1) {
-            $url = 'https://www.kdg.be/opleidingen/professionele-bachelor';
-            $nameCssSelector = 'article > header > h4 > a';
-            // $descriptionCssSelector = 'div.field-item > h3 + p';
-        }
-        elseif($school->id == 2) {
-            $url = 'https://www.uantwerpen.be/nl/onderwijs/opleidingsaanbod/';
-            $nameCssSelector = 'section > h2 > a';
-        }
-        elseif($school->id == 3) {
-            $url = 'http://www.thomasmore.be/opleidingen/zoeken?f[0]=im_field_opleidingstype%3A6&f[1]=im_field_opleidingstype%3A64';
-            $nameCssSelector = 'div.field > h2';
+        switch($school->id) {
+            case 1:
+                $url = 'https://www.kdg.be/opleidingen/professionele-bachelor';
+                $nameCssSelector = 'article > header > h4 > a';
+                break;
+            case 2:
+                $url = 'https://www.uantwerpen.be/nl/onderwijs/opleidingsaanbod/';
+                $nameCssSelector = 'section > h2 > a';
+                break;
+            case 3:
+                $url = 'http://www.thomasmore.be/opleidingen/zoeken?f[0]=im_field_opleidingstype%3A6&f[1]=im_field_opleidingstype%3A64';
+                $nameCssSelector = 'div.field > h2';
+                break;
+            case 4:
+                $url = 'https://www.ap.be/bachelors-en-masters/459';
+                $nameCssSelector = 'div.field-item > p > a';
+                break;
+            case 5:
+                $url = '';
+                $nameCssSelector = '';
+                break;
+            case 6:
+                $url = '';
+                $nameCssSelector = '';
+                break;
         }
 
         $crawlInfo = $this->getCrawlInfo($url);
@@ -75,6 +84,8 @@ class PageController extends Controller
         $scrapedNames = $crawlInfo['crawler']->filter($nameCssSelector)->extract($crawlInfo['text']);
         $scrapedCourseUrls = $crawlInfo['crawler']->filter($nameCssSelector)->extract($crawlInfo['href']);
         // $scrapedDescriptions = $crawlInfo['crawler']->filter($descriptionCssSelector)->extract($crawlInfo['text']);
+
+        // $scrapedCourseUrls = $this->getFullUris($crawlInfo, count($scrapedNames), $school->id);
 
         // Thomas More moet speciaal doen ze
         if($school->id == 3) {
@@ -84,6 +95,10 @@ class PageController extends Controller
             $link = $crawlInfo['crawler']->selectLink('3')->link();
             $crawlInfo['crawler'] = $crawlInfo['client']->click($link);
             $scrapedNames = array_merge($scrapedNames, $crawlInfo['crawler']->filter($nameCssSelector)->extract($crawlInfo['text']));
+        }
+        elseif($school->id == 4) {
+            $scrapedNames = array_slice($scrapedNames, 2);
+            $scrapedCourseUrls = array_slice($scrapedCourseUrls, 2);
         }
 
         for($i = 0; $i < count($scrapedNames); $i++) {
@@ -98,6 +113,17 @@ class PageController extends Controller
                 $urlFromName = str_replace(explode(',', '- en , en , & , '), array_fill(0, 4, '-'), $urlFromName);
                 $scrapedCourseUrls[$i] = 'http://www.thomasmore.be/ons-aanbod/' . $urlFromName;
             }
+            elseif($school->id == 4) {
+                if(strpos($scrapedCourseUrls[$i], 'ap.be') === false) {
+                    $scrapedCourseUrls[$i] = 'https://www.ap.be' . $scrapedCourseUrls[$i];
+                }
+            }
+            elseif($school->id == 5) {
+
+            }
+            elseif($school->id == 6) {
+
+            }
 
             $scrapedCourse = new Course;
             $scrapedCourse->school_id = $school->id;
@@ -109,12 +135,12 @@ class PageController extends Controller
 
 		return view('school', compact('school'));
 	}
-	public function opleiding(Course $course)
-	{
-		return view('opleiding', compact('course'));
-	}
-	public function nieuws()
-	{
+
+    // public function opleiding(Course $course) {
+    //     return view('opleiding', compact('course'));
+    // }
+
+	public function nieuws() {
         Article::where('scraped', true)->delete();
 
         $url = 'https://www.gate15.be/nl/nieuws';
@@ -147,18 +173,18 @@ class PageController extends Controller
 
 		return view('nieuws', compact('articles'));
 	}
-	public function artikel(Article $article)
-	{
+
+	public function artikel(Article $article) {
 		return view('artikel', compact('article'));
 	}
-	public function gids()
-	{
+
+	public function gids() {
         $free_time_items = FreeTime::all();
 
 		return view('gids', compact('free_time_items'));
 	}
-	public function zoeken()
-	{
+
+	public function zoeken() {
 		return view('zoek');
 	}
 
@@ -188,4 +214,25 @@ class PageController extends Controller
 
         return $crawlInfo;
     }
+
+    // public function getFullUris($crawlInfo, $scrapedNamesCount, $schoolId) {
+    //     $scrapedCourseUrls = array();
+
+    //     for($i = 1; $i <= $scrapedNamesCount; $i++) {
+    //         if($schoolId == 1) {
+    //             array_push(
+    //                 $scrapedCourseUrls,
+    //                 $crawlInfo['crawler']->filter("article:nth-of-type($i) > header > h4 > a")->link()->getUri()
+    //             );
+    //     }
+    //         elseif($schoolId == 2) {
+    //             array_push(
+    //                 $scrapedCourseUrls,
+    //                 $crawlInfo['crawler']->filter("h2:nth-of-type($i) > a")->link()->getUri()
+    //             );
+    //         }
+    //     }
+
+    //     return $scrapedCourseUrls;
+    // }
 }
