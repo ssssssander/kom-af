36f17209edc4622b3e38e4b26e5a9552966158dc
Sander Borret <sander.borret@student.kdg.be>
2017-05-11 12:13:53 +0200

    get articles from gate15 api

--- app/Console/Commands/ScrapeArticles.php
+++ app/Console/Commands/ScrapeArticles.php
@@ -39,23 +39,21 @@ class ScrapeArticles extends Command
      */
     public function handle()
     {
-        $url = 'https://www.gate15.be/srv/content/d/content-type/10/start/0/limit/11/excluded_tags/trots';
+        $url = 'https://www.gate15.be/srv/content/d/content-type/10/start/0/limit/100/excluded_tags/trots';
         $json = file_get_contents($url);
         $articles = json_decode($json);
 
-        var_dump($articles->data[0]);
-
-        $bar = $this->output->createProgressBar(count($scrapedTitles));
+        $bar = $this->output->createProgressBar(count($articles->data));
         // $scrapedArticleIds = Article::select('id')->where('user_id', null)->get();
 
         // if($scrapedArticleIds->isEmpty()) {
             for($i = 0; $i < count($articles->data); $i++) {
                 $scrapedArticle = new Article;
-                $scrapedArticle->title = $scrapedTitles[$i];
-                $scrapedArticle->content = $scrapedContent[$i];
-                $scrapedArticle->time_ago = $scrapedTimeAgoes[$i];
-                $scrapedArticle->image_url = $scrapedImageUrls[$i];
-                $scrapedArticle->article_url = $scrapedArticleUrls[$i];
+                $scrapedArticle->title = $articles->data[$i]->latestVersion->title;
+                $scrapedArticle->content = strip_tags($articles->data[$i]->latestVersion->snippets[1]->body->text);
+                $scrapedArticle->time_ago = $articles->data[$i]->latestVersion->createdAt;
+                $scrapedArticle->image_url = $articles->data[$i]->latestVersion->snippets[0]->body->file[0]->src;
+                $scrapedArticle->article_url = 'https://www.gate15.be/nl/nieuws/' . $articles->data[$i]->slug;
                 $scrapedArticle->save();
                 // Article::find($scrapedArticle->id)->delete();
 
--- database/seeds/HeroImagesTableSeeder.php
+++ database/seeds/HeroImagesTableSeeder.php
@@ -14,9 +14,9 @@ class HeroImagesTableSeeder extends Seeder
         DB::table('hero_images')->insert([
             ['template_name' => 'algemeen', 'hero_image_url' => 'algemeen.jpeg', 'video' => false],
             ['template_name' => 'testimonials', 'hero_image_url' => 'testimonials.jpeg', 'video' => false],
-            ['template_name' => 'scholen', 'hero_image_url' => 'scholen.jpg', 'video' => false],
+            ['template_name' => 'scholen', 'hero_image_url' => 'scholen.jpeg', 'video' => false],
             ['template_name' => 'nieuws', 'hero_image_url' => 'nieuws.jpeg', 'video' => false],
-            ['template_name' => 'gids', 'hero_image_url' => 'gids.jpg', 'video' => false],
+            ['template_name' => 'gids', 'hero_image_url' => 'gids.jpeg', 'video' => false],
             ['template_name' => 'zoeken', 'hero_image_url' => 'zoeken.jpeg', 'video' => false]
         ]);
     }
