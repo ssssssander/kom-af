512079883699c0f5e250c17eaafed50e33cf7014
TVke <thomas.verhelst@student.kdg.be>
2017-06-03 19:28:12 +0200

    niews style finisched

--- resources/views/nieuws.blade.php
+++ resources/views/nieuws.blade.php
@@ -5,16 +5,15 @@
         <article>
             @if($article->user_id == null)
                 <a href="{{ $article->article_url }}">
+					<div></div>
+					<h2>{{ $article->title }}</h2>
                     <img src="{{ $article->image_url }}" alt="{{ $article->title }}">
             @else
                 <a href="{{ route('artikel', ['article' => $article->id]) }}">
+					<div></div>
+					<h2>{{ $article->title }}</h2>
                     <img src="{{ asset('img/' . $article->image_url) }}" alt="{{ $article->title }}">
             @endif
-                <h5>{{ $article->title }}</h5>
-                @if($article->user_id != null)
-                    <p>Door {{ $article->user->first_name . ' ' . $article->user->last_name }}</p>
-                @endif
-                <time>{{ $article->time_ago }} geleden</time>
                 <p>{{ $article->content }}</p>
             </a>
         </article>
